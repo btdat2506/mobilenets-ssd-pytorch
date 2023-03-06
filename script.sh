294f3d3b674300aec778d56afb111ff19ecbd1cc
Thành Đạt <44937407+btdat2506@users.noreply.github.com>
2023-03-06 08:37:39 +0000

    Update new Script file

---
+++ script.sh
@@ -0,0 +1,8 @@
+sudo add-apt-repository ppa:bashtop-monitor/bashtop
+sudo apt update
+sudo apt install bashtop
+
+pip install --user kaggle
+export KAGGLE_USERNAME=btdat2506
+export KAGGLE_KEY=
+kaggle datasets download ansarisaquib/bdd100
